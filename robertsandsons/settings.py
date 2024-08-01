a6cd88a8dc1aee9b7da8ac097cfb5df9c92d481c
mykki92 <mike2108@hotmail.co.uk>
2024-08-01 16:05:05 +0000

    Fix database connection

--- robertsandsons/settings.py
+++ robertsandsons/settings.py
@@ -66,16 +66,20 @@ WSGI_APPLICATION = 'robertsandsons.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#   'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-#}
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+   'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres.oowafmlycmzkuqkewtou',
+        'PASSWORD': 'JamesMikeWillsTommy',
+        'HOST': 'aws-0-eu-west-2.pooler.supabase.com',
+        'PORT': '6543',
+    }
 }
+
+#DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+#}
 
 
 # Password validation
